8f29200d1842dd51ec346ac625fc07c9be09d4e7
mackudela <56248505+mackudela@users.noreply.github.com>
2022-05-30 21:59:35 +0200

    Revert "Merge itemDropper to merge&Testing"

--- BR2DGame/Assets/Scripts/Box.cs
+++ BR2DGame/Assets/Scripts/Box.cs
@@ -6,30 +6,14 @@ using Photon.Pun;
 public class Box : MonoBehaviour
 {
     [SerializeField] private int health = 100;
-    [SerializeField] private GameObject akPrefab;
-    [SerializeField] private GameObject pistolPrefab;
-    private int dropNumber;
+
     public void TakeDamage(int damage)
     {
         health -= damage;
 
         if(health <= 0)
         {
-            dropItems();
             Die();
-        }
-    }
-
-    // drop items after the box is destroyed
-    private void dropItems() {
-        dropNumber = Random.Range(1, 3);
-        switch (dropNumber) {
-            case 1:
-                PhotonNetwork.Instantiate(pistolPrefab.name, this.transform.position, this.transform.rotation);
-                break;
-            case 2:
-                PhotonNetwork.Instantiate(akPrefab.name, this.transform.position, this.transform.rotation);
-                break;
         }
     }
 
