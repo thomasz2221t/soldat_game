f8ec6cc23394d3d91b0ec032ee145b76e5032897
thomasz2221t <56248548+thomasz2221t@users.noreply.github.com>
2022-05-30 18:42:33 +0200

    Fix dmg bug #1

--- BR2DGame/Assets/Scripts/Bullet.cs
+++ BR2DGame/Assets/Scripts/Bullet.cs
@@ -40,7 +40,7 @@ public class Bullet : MonoBehaviourPun
     {
         bool hit = false;
         Box destroyable = collision.GetComponent<Box>();
-        Player playerBody = collision.GetComponent<Player>();
+        //Player playerBody = collision.GetComponent<Player>();
 
         if (destroyable != null)
         {
@@ -48,9 +48,9 @@ public class Bullet : MonoBehaviourPun
             hit = true;
         }
 
-        if((playerBody != null)&&(!collision.gameObject.GetPhotonView().IsMine))
+        if((collision.gameObject != null)&&(!collision.gameObject.GetPhotonView().IsMine))
         {
-            playerBody.TakeDamage(damage);
+            collision.GetComponent<Player>().TakeDamage(damage);
             hit = true;
         }
 
