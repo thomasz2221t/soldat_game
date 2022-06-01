6178ff6eed0d2a24cb445904d36d3301711d9f97
mackudela <56248505+mackudela@users.noreply.github.com>
2022-06-01 22:31:56 +0200

    Clean-up of the unused code

--- BR2DGame/Assets/Scripts/Minimap.cs
+++ BR2DGame/Assets/Scripts/Minimap.cs
@@ -6,22 +6,8 @@ using UnityEngine;
 public class Minimap : MonoBehaviour
 {
 
-    //private GameObject localPlayer;
-
     private void Start() {
-        //this.transform.position = new Vector3(0,0,0);
 
-
-        /*GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        foreach (GameObject player in players) {
-            if (PhotonView.Get(player).IsMine) {
-                this.localPlayer = player;
-                Debug.Log("Znalaz³em gracza w minimap script");
-                break;
-            }
-        }
-
-        if (localPlayer == null) Debug.Log("DUPAAAAAAAAAAAA");*/
     }
 
     private void LateUpdate() {
@@ -29,20 +15,6 @@ public class Minimap : MonoBehaviour
             Vector3 newPosition = SpawnPlayers.getLocalPlayerReference().transform.position;
             newPosition.z = transform.position.z;
             transform.position = newPosition;
-
-            //transform.rotation = Quaternion.Euler(0f, 0f, SpawnPlayers.getLocalPlayerReference().transform.eulerAngles.y);
-            //transform.rotation = Quaternion.Euler(0f, SpawnPlayers.getLocalPlayerReference().transform.eulerAngles.y, 0f);
         }
-
-        /*if (localPlayer != null) {
-
-        Vector3 newPosition = localPlayer.transform.position;
-        newPosition.z = transform.position.z;
-        transform.position = newPosition;
-
-        //transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
-        transform.rotation = Quaternion.Euler(0f, localPlayer.transform.eulerAngles.y, 0f);
-    }*/
     }
-
 }
--- BR2DGame/Assets/Scripts/cameraController.cs
+++ BR2DGame/Assets/Scripts/cameraController.cs
@@ -5,22 +5,10 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour
 {
-    
     [SerializeField] PhotonView view;
-    //private GameObject localPlayer;
-    //private Transform target;
 
     // Start is called before the first frame update
     private void Start() {
-        /*GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        foreach (GameObject player in players) {
-            if (PhotonView.Get(player).IsMine) {
-                this.target = player.transform;
-                Debug.Log("Znalaz³em gracza w camera controller");
-                break;
-            }
-        }*/
-
         
     }
 }
